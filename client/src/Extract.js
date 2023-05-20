76bcf46e845dead56dd0828936e1cdd5a2e83ea4
Christie-Carol Beauchamp <christiecarolbeauchamp@gmail.com>
2023-05-20 16:04:11 -0700

    Remove extra lines

--- client/src/Extract.js
+++ client/src/Extract.js
@@ -1,46 +1,32 @@
-import React, { useState } from 'react';
+const handlePdfUpload = async (event) => {
+  const file = event.target.files[0];
 
-const PdfTextExtraction = () => {
-  const [extractedText, setExtractedText] = useState('');
+  // Read the PDF file as bytes
+  const reader = new FileReader();
+  reader.onloadend = async () => {
+    const fileData = reader.result;
 
-  const handlePdfUpload = async (event) => {
-    const file = event.target.files[0];
-  
-    // Read the PDF file as bytes
-    const reader = new FileReader();
-    reader.onloadend = async () => {
-      const fileData = reader.result;
-  
-      // Prepare the request body with base64-encoded file data
-      const requestBody = {
-        file_data: btoa(fileData),
-      };
-  
-      try {
-        const response = await fetch('http://localhost:8000/extract-text', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(requestBody),
-        });
-    
-        const data = await response.json();
-        console.log(data.extracted_text)
-        setExtractedText(data.extracted_text);
-      } catch (error) {
-        console.error('Text extraction failed:', error);
-      }
+    // Prepare the request body with base64-encoded file data
+    const requestBody = {
+      file_data: btoa(fileData),
     };
-    reader.readAsBinaryString(file);
+
+    try {
+      const response = await fetch("http://localhost:8000/extract-text", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(requestBody),
+      });
+
+      const data = await response.json();
+      console.log(data.extracted_text);
+    } catch (error) {
+      console.error("Text extraction failed:", error);
+    }
   };
-  
+  reader.readAsBinaryString(file);
+};
 
-  return (
-    <div>
-      <input type="file" accept=".pdf" onChange={handlePdfUpload} />
-    </div>
-  );
-}
-
-export default PdfTextExtraction;
+export default handlePdfUpload;
