5a60eb21dafdef1aa0c61f6a190536369f513f81
Christie-Carol Beauchamp <christiecarolbeauchamp@gmail.com>
2023-05-20 15:08:56 -0700

    Styling form upload

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,7 +1,11 @@
 import React from "react";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 
 function Form() {
+  const [files, setFiles] = useState({});
+  const [name, setName] = useState();
+
   const { endpoint } = useParams();
   var title = endpoint.charAt(0).toUpperCase() + endpoint.slice(1);
   return (
@@ -9,7 +13,15 @@ function Form() {
       <h1>{title}: Case Information</h1>
       <h2>Add Plaintiff Information</h2>
       <div className="caseForm">
+        <label>Plaintiff Name</label>
         <input name="Plaintiff Name" />
+        <div className="fileUploadSection">
+          <label>Filing Upload</label>
+          <button className="uploadButton" type="button">
+            <span> Upload {"files"}</span>
+          </button>
+          <input type="file" title="" value="" hidden />
+        </div>
       </div>
     </div>
   );
