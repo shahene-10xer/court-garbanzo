c05e54f373d5a96704e8c347d87b9e5dbb40801b
Christie-Carol Beauchamp <christiecarolbeauchamp@gmail.com>
2023-05-20 14:41:00 -0700

    Basic UI setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,18 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from './components/Header.js';
+import Home from './components/Home.js';
+import Form from './components/Form.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Header />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/form/:endpoint" element={<Form />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
